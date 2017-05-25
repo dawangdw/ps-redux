7544f84bf1303dca46d8c7bbecf613c0c1662bbf
Michael Ta <michaelta@Michaels-Macbook.local>
2017-05-25 16:22:52 -0400

    Broken

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,10 +5,29 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS };
+}
+
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS };
+}
+
 export function loadCourses() {
   return function (dispatch) {
     return courseAPI.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw (error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseAPI.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+        dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
       throw (error);
     });
--- src/components/common/SelectInput.js
+++ src/components/common/SelectInput.js
@@ -4,7 +4,20 @@ const SelectInput = ({ name, label, onChange, defaultOption, value, error, optio
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-
+      <div className="field">
+        <select
+          name={name}
+          value={value}
+          onChange={onChange}
+          className="form-control">
+          <option value="">{defaultOption}</option>
+          {options.map((option) => {
+            return <option key={option.value} value={option.value}>{option.text}</option>;
+          })
+          }
+        </select>
+        {error && <div className="alert alert-danger">{error}</div>}
+      </div>
     </div>
   );
 };
